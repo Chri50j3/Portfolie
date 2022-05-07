9137d5be66b6e05b0e0892390fb5f1d50dd02953
Chri50j3 <christopher@bik.dk>
2022-05-07 15:15:25 +0200

    slides

--- index.php
+++ index.php
@@ -65,6 +65,28 @@ echo "\t </div>
 
 echo "</table>\n";
 
+echo "<div id='carouselExampleControls' class='carousel slide' data-bs-ride='carousel'>
+<div class='carousel-inner'>
+  <div class='carousel-item active'>
+    <img src='https://raw.githubusercontent.com/Chri50j3/Portfolie/main/Slide1.png' class='d-block w-100' alt='...'>
+  </div>
+  <div class='carousel-item'>
+    <img src='...' class='d-block w-100' alt='...'>
+  </div>
+  <div class='carousel-item'>
+    <img src='...' class='d-block w-100' alt='...'>
+  </div>
+</div>
+<button class='carousel-control-prev' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+  <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+  <span class='visually-hidden'>Previous</span>
+</button>
+<button class='carousel-control-next' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+  <span class='carousel-control-next-icon' aria-hidden='true'></span>
+  <span class='visually-hidden'>Next</span>
+</button>
+</div>";
+
 // Free resultset
 mysqli_free_result($result);
 

dbd402057babefe25b76414813f17556bd51f77b
Chri50j3 <christopher@bik.dk>
2022-08-12 14:12:50 +0200

    VideoPlayer

--- Animation/index.php
+++ Animation/index.php
@@ -18,7 +18,7 @@ echo "\t
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
           integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
 
-		  <title> 3D modellering</title>
+		  <title>Animation</title>
 </head>
 <body>
 
@@ -39,7 +39,7 @@ while ($line = mysqli_fetch_row($result))
 	echo "\t <div class='col'>
 		<div class='card' style='width: 18rem;'>
             <video controls>
-			    <source src=$line[2] type=´video/mp4´ class='card-img-top' alt='...'>
+			    <source src='$line[0].mp4' type='video/mp4' class='card-img-top' alt='...'>
             </video>
 			<div class='card-body'>
 				<h5 class='card-title'>$line[0]</h5>

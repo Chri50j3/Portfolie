475b81cb39f8bf2ce504ae287714d332436a21e2
Chri50j3 <christopher@bik.dk>
2022-12-05 16:53:15 +0100

    420

--- Spiludvikling/index.php
+++ Spiludvikling/index.php
@@ -8,27 +8,56 @@ mysqli_select_db($link, 'bik_dkportfolio') or die('Could not select database');
 $query = 'SELECT * FROM Spiludvikling';
 $result = mysqli_query($link, $query) or die('Query failed: ' . mysql_error());
 
-echo "\t
-<html lang='en'>
+echo '\t
+<html lang="en">
 <head>
     <!-- Required meta tags -->
-    <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
-          integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 		  <title>Game Design</title>
 </head>
-<body>
+<body style="background-color:rgb(14, 49, 68);">
 
-<!-- As a link -->
-<nav class='navbar navbar-light bg-light'>
-    <div class='container-fluid'>
-        <a class='navbar-brand' href='/'>Christopher's Portfolio</a>
+<nav class="navbar navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Christopher Pontoppidan Bik</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Christopher Pontoppidan Bik</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.linkedin.com/in/christopher-pontoppidan-bik-518962185/">LinkedIn</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="https://bik.dk/3D/">3D Modeling</a></li>
+              <li><a class="dropdown-item" href="https://bik.dk/Animations/">Animations & CGI</a></li>
+              <li><a class="dropdown-item" href="https://bik.dk/Game/">Game Design</a></li>
+              <li><a class="dropdown-item" href="https://bik.dk/Marketing/">Marketing</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </nav>
-<div class='container'> \n";
+<div class="container"> \n';
 
 
 // Printing results in HTML
@@ -36,8 +65,8 @@ echo  "\t<table>\n";
 echo  "\t<div class='row'>\n";
 while ($line = mysqli_fetch_row($result))
 {
-	echo "\t <div class='col'>
-		<div class='card' style='width: 18rem;'>
+	echo "\t <div class='col-md-6 offset-md-3'>
+		<div class='card' style='width: 38rem;'>
 			<img src=$line[2] class='card-img-top' alt='...'>
 			<div class='card-body'>
 				<h5 class='card-title'>$line[0]</h5>
